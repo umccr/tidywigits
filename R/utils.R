4ed0f76e98bd33b86699c11b47dc1ccb1ec84806
pdiakumis <peterdiakumis@gmail.com>
2025-04-27 12:05:47 +1000

    move to utils.R

--- R/utils.R
+++ R/utils.R
@@ -77,3 +77,31 @@ parse_file <- function(fname, schema, type) {
   )
   return(d[])
 }
+#' List Files
+#'
+#' Lists files in a given directory.
+#'
+#' @param d Path to directory.
+#' @param max_files Max files returned.
+#'
+#' @return A tibble with file basename, size, last modification timestamp
+#' and full path.
+#' @examples
+#' d <- system.file("R", package = "tidywigits")
+#' x <- list_files_dir(d)
+#' @testexamples
+#' expect_equal(names(x), c("bname", "size", "lastmodified", "path"))
+#' @export
+list_files_dir <- function(d, max_files = NULL) {
+  d <- fs::dir_info(path = d, recurse = TRUE, type = "file") |>
+    dplyr::mutate(
+      bname = basename(.data$path),
+      lastmodified = .data$modification_time
+    ) |>
+    dplyr::select("bname", "size", "lastmodified", "path")
+  if (!is.null(max_files)) {
+    d <- d |>
+      dplyr::slice_head(n = max_files)
+  }
+  d
+}
