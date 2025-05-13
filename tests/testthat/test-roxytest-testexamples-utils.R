558ac175ddffefbce4ae89f970f17af9779c6029
pdiakumis <peterdiakumis@gmail.com>
2025-05-13 15:48:16 +1000

    add man/

--- tests/testthat/test-roxytest-testexamples-utils.R
+++ tests/testthat/test-roxytest-testexamples-utils.R
@@ -2,7 +2,7 @@
 
 # File R/utils.R: @testexamples
 
-test_that("Function list_files_dir() @ L110", {
+test_that("Function list_files_dir() @ L111", {
   
   d <- system.file("R", package = "tidywigits")
   x <- list_files_dir(d)
