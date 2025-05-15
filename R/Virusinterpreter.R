7a3073125037598df18cc582a0acc34afa1ad0e9
pdiakumis <peterdiakumis@gmail.com>
2025-05-15 22:47:56 +1000

    virusinterpreter: handle blacklisted column in v1.5

--- R/Virusinterpreter.R
+++ R/Virusinterpreter.R
@@ -22,15 +22,42 @@ Virusinterpreter <- R6::R6Class(
     #' Output directory of tool.
     initialize = function(path) {
       super$initialize(name = "virusinterpreter", path = path)
-      self$tidy = super$.tidy(envir = self)
+      # self$tidy = super$.tidy(envir = self)
     },
 
     #' @description Read `virus.annotated.tsv` file.
     #' @param x (`character(1)`)\cr
     #' Path to file.
     parse_annotated = function(x) {
-      schema <- self$config$.raw_schema("annotated")
+      # TODO: refactor into separate func
+      hdr <- readr::read_tsv(
+        x,
+        col_types = readr::cols(.default = "c"),
+        n_max = 0
+      ) |>
+        colnames()
+      schemas_all <- self$config$.raw_schemas_all() |>
+        dplyr::filter(name == "annotated")
+      schema <- schemas_all |>
+        dplyr::select(version, schema) |>
+        dplyr::rowwise() |>
+        dplyr::mutate(
+          length_match = length(hdr) == nrow(.data$schema),
+          all_match = if (length_match) {
+            all(hdr == .data$schema[["field"]])
+          } else {
+            FALSE
+          }
+        ) |>
+        dplyr::filter(.data$all_match) |>
+        dplyr::ungroup()
+      assertthat::assert_that(nrow(schema) == 1)
+      version <- schema$version
+      schema <- schema |>
+        dplyr::select("schema") |>
+        tidyr::unnest("schema")
       d <- parse_file(x, schema, type = "tsv")
+      attr(d, "file_version") <- version
       d
     },
     #' @description Tidy `virus.annotated.tsv` file.
@@ -38,7 +65,8 @@ Virusinterpreter <- R6::R6Class(
     #' Path to file.
     tidy_annotated = function(x) {
       raw <- self$parse_annotated(x)
-      schema <- self$config$.tidy_schema("annotated")
+      version <- attr(raw, "file_version")
+      schema <- self$config$.tidy_schema("annotated", v = version)
       colnames(raw) <- schema[["field"]]
       list(annotated = raw) |>
         tibble::enframe(value = "data")
