6387e71f637f7eb89c376feb2cdba877f7e47185
pdiakumis <peterdiakumis@gmail.com>
2025-05-23 18:04:25 +1000

    file_to_yaml: add triple quotes for easier replacement

--- inst/scripts/file_to_yaml.R
+++ inst/scripts/file_to_yaml.R
@@ -1,16 +1,17 @@
-x <- "~/projects/tidywigits/nogit/oa_2.0.0_colo829_20250507/bamtools/COLO829v003_WGTS_COLO829_tumor_bamtools/COLO829_tumor.bam_metric.summary.tsv"
-y <- readr::read_tsv(x)
-purrr::map_chr(y, class) |>
+"~/projects/tidywigits/nogit/oa_v1/isofox/L2500527.isf.gene_collection.csv" |>
+  readr::read_csv() |>
+  purrr::map_chr(class) |>
   tibble::enframe(name = "field", value = "type") |>
   dplyr::mutate(
     type = dplyr::case_match(
       .data$type,
-      "character" ~ "char",
-      "integer" ~ "int",
-      "numeric" ~ "float",
-      "logical" ~ "char"
+      "character" ~ "'char'",
+      "integer" ~ "'int'",
+      "numeric" ~ "'float'",
+      "logical" ~ "'char'"
     ),
-    n = dplyr::row_number()
+    n = dplyr::row_number(),
+    field = paste0("'", .data$field, "'")
   ) |>
   tidyr::nest(.by = n, .key = "latest") |>
   dplyr::select(-"n") |>
