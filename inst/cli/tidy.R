1b75876ccdebf1fc00a74df00ebf5b80fca9e32a
pdiakumis <peterdiakumis@gmail.com>
2025-06-19 00:46:57 +1000

    cli: add db support

--- inst/cli/tidy.R
+++ inst/cli/tidy.R
@@ -1,10 +1,12 @@
 # fmt: skip
 tidy_add_args <- function(subp) {
   tidy <- subp$add_parser("tidy", help = "Tidy WiGiTS Workflow Outputs")
-  tidy$add_argument("-d", "--in_dir", help = glue("{emoji('tent')} Directory with untidy WiGiTS workflow results."), required = TRUE)
-  tidy$add_argument("-o", "--out_dir", help = glue("{emoji('rocket')} Directory to output tidy results (if writing to file system)."))
-  tidy$add_argument("-f", "--format", help = glue("{emoji('art')} Format of output. Default: %(default)s."), default = "parquet")
-  tidy$add_argument("-i", "--id", help = glue("{emoji('triangular_flag_on_post')} ID to use for nemo run."), required = TRUE)
+  tidy$add_argument("-d", "--in_dir", help = glue("{emoji('snail')} Input directory."), required = TRUE)
+  tidy$add_argument("-o", "--out_dir", help = glue("{emoji('rocket')} Output directory."))
+  tidy$add_argument("-f", "--format", help = glue("{emoji('art')} Format of output (def: %(default)s)."), default = "parquet")
+  tidy$add_argument("-i", "--id", help = glue("{emoji('triangular_flag_on_post')} ID to use for this run."), required = TRUE)
+  tidy$add_argument("--dbname", help = glue("{emoji('dog')} Database name (def: %(default)s)."), default = "nemo")
+  tidy$add_argument("--dbuser", help = glue("{emoji('turtle')} Database user (def: %(default)s)."), default = "orcabus")
   tidy$add_argument("-q", "--quiet", help = glue("{emoji('sleeping')} Shush all the logs."), action = "store_true")
 }
 
@@ -15,7 +17,9 @@ tidy_parse_args <- function(args) {
     in_dir = args$in_dir,
     out_dir = out_dir,
     out_format = args$format,
-    id = args$id
+    id = args$id,
+    dbname = args$dbname,
+    dbuser = args$dbuser
   )
 
   # tidy run
@@ -26,7 +30,15 @@ tidy_parse_args <- function(args) {
   }
 }
 
-nemo_tidy <- function(in_dir, out_dir, out_format, id) {
+nemo_tidy <- function(in_dir, out_dir, out_format, id, dbname, dbuser) {
+  dbconn <- NULL
+  if (out_format == "db") {
+    dbconn <- DBI::dbConnect(
+      drv = RPostgres::Postgres(),
+      dbname = dbname,
+      user = dbuser
+    )
+  }
   e <- emojifont::emoji
   cli::cli_alert_info(
     "{date_log()} {e('tropical_fish')}: Tidying Oncoanalyser dir: {in_dir}"
@@ -35,10 +47,13 @@ nemo_tidy <- function(in_dir, out_dir, out_format, id) {
   res <- oa$nemofy(
     odir = out_dir,
     format = out_format,
-    id = id
+    id = id,
+    dbconn = dbconn
   )
-  cli::cli_alert_success(
-    "{date_log()} {e('tada')}: Tidy results at: {out_dir}"
-  )
+  if (out_format == "db") {
+    cli::cli_alert_success("{date_log()} {e('tada')}: Tidy results written to db: {dbname}")
+  } else {
+    cli::cli_alert_success("{date_log()} {e('tada')}: Tidy results written to dir: {out_dir}")
+  }
   return(invisible(res))
 }
