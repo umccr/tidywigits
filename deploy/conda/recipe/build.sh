650aac071068719fba73078815dc02384bc56dc1
pdiakumis <peterdiakumis@gmail.com>
2025-07-04 13:32:14 +1000

    conda: refactor build.sh

--- deploy/conda/recipe/build.sh
+++ deploy/conda/recipe/build.sh
@@ -4,6 +4,7 @@ export DISABLE_AUTOBREW=1
 R CMD INSTALL --build .
 
 # Copy CLI to conda bin
-mkdir -p ${PREFIX}/bin
-cp ${SRC_DIR}/inst/cli/*.R ${PREFIX}/bin
-chmod +x ${PREFIX}/bin/tidywigits.R
+BIN_DIR="${PREFIX}/bin"
+mkdir -p ${BIN_DIR}
+cp ${SRC_DIR}/inst/cli/*.R ${BIN_DIR}
+chmod +x ${BIN_DIR}/tidywigits.R
