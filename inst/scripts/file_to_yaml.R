d9646e17fdfb62facdb95f44fc154c136c638e30
pdiakumis <peterdiakumis@gmail.com>
2025-09-20 11:23:40 +1000

    neo: add raw + tidy configs

--- inst/scripts/file_to_yaml.R
+++ inst/scripts/file_to_yaml.R
@@ -1,29 +1,17 @@
-d1 <- here::here("nogit/oa_v2/esvee")
-pref <- "COLO829_tumor.esvee"
-tool <- "esvee"
-descr <- "Structural variant caller optimised for short read sequencing."
+d1 <- here::here(
+  "../biodaily/pdiakumis/nemo/accreditation_wgs/nogit/oncoanalyser-wgts-dna/202509100539df0e/L2101100__L2101099/neo"
+)
+pref <- "L2101100.neo"
+tool <- "neo"
+descr <- "Neoepitope prediction."
 # fmt: skip
 d <- tibble::tribble(
     ~name,               ~descr,                   ~pat,                                       ~path,
-    "prepfraglen",       "Fragment length stats.", "\\.esvee\\.prep\\.fragment_length\\.tsv$", glue("prep/{pref}.prep.fragment_length.tsv"),
-    "prepdiscstats",     "Discordant read stats.", "\\.esvee\\.prep\\.disc_stats\\.tsv$",      glue("prep/{pref}.prep.disc_stats.tsv"),
-    "prepjunction",      "Candidate junctions.",   "\\.esvee\\.prep\\.junction\\.tsv$",        glue("prep/{pref}.prep.junction.tsv"),
-    "assemblephased",    "Phased assemblies.",     "\\.esvee\\.phased_assembly\\.tsv$",        glue("assemble/{pref}.phased_assembly.tsv"),
-    "assembleassembly",  "Breakend assemblies.",   "\\.esvee\\.assembly\\.tsv$",               glue("assemble/{pref}.assembly.tsv"),
-    "assemblebreakend",  "Breakends.",             "\\.esvee\\.breakend\\.tsv$",               glue("assemble/{pref}.breakend.tsv"),
-    "assemblealignment", "Realigned assemblies.",  "\\.esvee\\.alignment\\.tsv$",              glue("assemble/{pref}.alignment.tsv")
+    "neocand", "Neoepitope candidates.", "\\.neo\\.neo_data\\.tsv$", glue("finder/{pref}.neo_data.tsv"),
+    "neopred", "Neoepitope predictions.", "\\.neo\\.neoepitope\\.tsv$", glue("scorer/{pref}.neoepitope.tsv")
   ) |>
   dplyr::mutate(type = "tsv", path = file.path(d1, .data$path))
-config_prep_multi(d, tool_descr = descr) |>
-  config_prep_write(
+nemo::config_prep_multi(d, tool_descr = descr) |>
+  nemo::config_prep_write(
     here::here(glue::glue("inst/config/tools/{tool}/raw.yaml"))
   )
-
-# fmt: skip
-d <- tibble::tribble(
-    ~name, ~descr, ~pat, ~path, ~type,
-    "neoepitope", "Gene fusion neoepitopes.", "\\.linx\\.neoepitope\\.tsv$", here::here("nogit/oa_v2/linx/somatic_annotations/COLO829_tumor.linx.neoepitope.tsv"), "tsv",
-)
-config_prep_multi(d, tool_descr = "linx") |>
-  yaml::as.yaml(column.major = F) |>
-  cat()
