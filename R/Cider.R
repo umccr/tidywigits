cf819886e8191132c1fd1dd6d04d14ff08e5d65d
pdiakumis <peterdiakumis@gmail.com>
2025-09-21 16:50:05 +1000

    Cider support

---
+++ R/Cider.R
@@ -0,0 +1,66 @@
+#' @title Cider Object
+#'
+#' @description
+#' Cider file parsing and manipulation.
+#' @examples
+#' cls <- Cider
+#' indir <- system.file("extdata/oa", package = "tidywigits")
+#' odir <- tempdir()
+#' id <- "cider_run1"
+#' obj <- cls$new(indir)
+#' obj$nemofy(odir = odir, format = "parquet", id = id)
+#' (lf <- list.files(odir, pattern = "cider.*parquet", full.names = FALSE))
+#' @testexamples
+#' expect_equal(length(lf), 3)
+#' @export
+Cider <- R6::R6Class(
+  "Cider",
+  inherit = Tool,
+  public = list(
+    #' @description Create a new Cider object.
+    #' @param path (`character(1)`)\cr
+    #' Output directory of tool. If `files_tbl` is supplied, this basically gets
+    #' ignored.
+    #' @param files_tbl (`tibble(n)`)\cr
+    #' Tibble of files from [nemo::list_files_dir()].
+    initialize = function(path = NULL, files_tbl = NULL) {
+      super$initialize(name = "cider", pkg = pkg_name, path = path, files_tbl = files_tbl)
+    },
+    #' @description Read `blastn_match.tsv.gz` file.
+    #' @param x (`character(1)`)\cr
+    #' Path to file.
+    parse_blastn = function(x) {
+      self$.parse_file(x, "blastn")
+    },
+    #' @description Tidy `blastn_match.tsv.gz` file.
+    #' @param x (`character(1)`)\cr
+    #' Path to file.
+    tidy_blastn = function(x) {
+      self$.tidy_file(x, "blastn")
+    },
+    #' @description Read `locus_stats.tsv` file.
+    #' @param x (`character(1)`)\cr
+    #' Path to file.
+    parse_locstats = function(x) {
+      self$.parse_file(x, "locstats")
+    },
+    #' @description Tidy `locus_stats.tsv` file.
+    #' @param x (`character(1)`)\cr
+    #' Path to file.
+    tidy_locstats = function(x) {
+      self$.tidy_file(x, "locstats")
+    },
+    #' @description Read `vdj.tsv.gz` file.
+    #' @param x (`character(1)`)\cr
+    #' Path to file.
+    parse_vdj = function(x) {
+      self$.parse_file(x, "vdj")
+    },
+    #' @description Tidy `vdj.tsv.gz` file.
+    #' @param x (`character(1)`)\cr
+    #' Path to file.
+    tidy_vdj = function(x) {
+      self$.tidy_file(x, "vdj")
+    }
+  )
+)
--- inst/scripts/file_to_yaml.R
+++ inst/scripts/file_to_yaml.R
@@ -7,15 +7,16 @@
   use("nemo", c("config_prep_multi", "config_prep_write"))
 }
 
-tool <- "teal"
+tool <- "cider"
 pref <- "sample1"
-descr <- "Telomere characterisation."
+descr <- "Lists CDR3 sequences for each of the IG and TCR loci, including an abundance estimate, incomplete rearrangements and IGK-KDE deletions."
 d1 <- here(glue("inst/extdata/oa/{tool}"))
 # fmt: skip
 d <- tribble(
     ~name,    ~descr,                  ~pat,               ~path,
-    "breakend", "Telomeric rearrangements.", "\\.teal\\.breakend\\.tsv\\.gz$", glue("{pref}.teal.breakend.tsv.gz"),
-    "tellength", "Telomeric length and content.", "\\.teal\\.tellength\\.tsv$", glue("{pref}.teal.tellength.tsv"),
+    "blastn", "BLASTn hits.", "\\.cider\\.blastn_match\\.tsv\\.gz$", glue("{pref}.cider.blastn_match.tsv.gz"),
+    "locstats", "Locus stats.", "\\.cider\\.locus_stats\\.tsv$", glue("{pref}.cider.locus_stats.tsv"),
+    "vdj", "VDJ sequences.", "\\.cider\\.vdj\\.tsv.gz$", glue("{pref}.cider.vdj.tsv.gz"),
   ) |>
   mutate(type = "tsv", path = file.path(d1, .data$path))
 stopifnot(all(file.exists(d$path)))
