da8a3b8cd460c890a594b041810f85e2656ce0aa
pdiakumis <peterdiakumis@gmail.com>
2025-10-05 23:45:19 +1100

    webqmd: view purple gene cnv tab

--- inst/website/boxbox/wigits/table.R
+++ inst/website/boxbox/wigits/table.R
@@ -8,3 +8,28 @@ down_button <- function(d, nm) {
     button_type = "primary"
   )
 }
+
+#' @export
+view_tbl <- function(x, pagination = FALSE, ...) {
+  reactable::reactable(
+    x,
+    sortable = TRUE,
+    searchable = TRUE,
+    pagination = pagination,
+    paginationType = "jump",
+    filterable = TRUE,
+    striped = TRUE,
+    highlight = TRUE,
+    bordered = TRUE,
+    showSortable = TRUE,
+    showPageSizeOptions = TRUE,
+    showPagination = TRUE,
+    theme = reactable::reactableTheme(
+      borderColor = "#dfe2e5",
+      stripedColor = "#f8f9fa",
+      highlightColor = "#f0f5ff",
+      cellPadding = "12px 15px"
+    ),
+    ...
+  )
+}
